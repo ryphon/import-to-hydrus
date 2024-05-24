889e74f163accef343368af69101875504db6bdb
ryphon <dylan@shepard.dev>
2024-05-24 00:00:00 +0000

    basic env/file for secrets

--- hydrus_node.py
+++ hydrus_node.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 import json
 import hydrus_api
@@ -10,6 +11,8 @@ from PIL.PngImagePlugin import PngInfo
 from PIL import Image
 
 REQUIRED_PERMISSIONS = (hydrus_api.Permission.IMPORT_FILES, hydrus_api.Permission.ADD_TAGS)
+hydrus_key = os.environ.get("HYDRUS_KEY")
+hydrus_url = os.environ.get("HYDRUS_URL")
 
 # image I/O
 def get_timestamp(time_format="%Y-%m-%d-%H%M%S"):
@@ -20,6 +23,35 @@ def get_timestamp(time_format="%Y-%m-%d-%H%M%S"):
         timestamp = now.strftime("%Y-%m-%d-%H%M%S")
 
     return(timestamp)
+
+def get_hydrus_client():
+    global hydrus_key
+    global hydrus_url
+    if hydrus_key is not None and hydrus_url is not None:
+        return hydrus_api.Client(hydrus_key, hydrus_url)
+
+    # Check for API key in file as a backup, not recommended
+    # Example:
+    # hydrus_api.txt
+    #{
+    #    "hydrus_key": "0150d9c4f6a6d2082534a997f4588dcf0c56dffe1d03ffbf98472236112236ae",
+    #    "hydrus_url": "http://localhost:45869"
+    #}
+    try:
+        if not hydrus_key and not hydrus_url:
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            with open(os.path.join(dir_path, "hydrus_api.txt"), "r") as f:
+                keys = json.loads(f.read().strip())
+                hydrus_key = keys['hydrus_key']
+                hydrus_url = keys['hydrus_url']
+            # Validate the key is not empty
+            if keys.strip() == "":
+                raise Exception(f"API Key is required to save the image outputs to Hydrus. \n PLease set the HYDRUS_API_KEY environment variable to your API key, \n and HYDRUS_API_URL to your API URL or place in {dir_path}/hydrus_api_key.txt.")
+
+    except Exception as e:
+        print(e)
+
+    return hydrus_api.Client(hydrus_key, hydrus_url)
 
 class Hydrus:
     def __init__(self):
@@ -32,13 +64,12 @@ class Hydrus:
                         "images": ("IMAGE", ),
                     },
                     "optional": {
-                        "hydrus_api_key": ("STRING",),
-                        "hydrus_api_url": ("STRING",),
                         "positive": ("STRING",{ "multiline": True, "forceInput": True}, ),
                         "negative": ("STRING",{"multiline": True, "forceInput": True}, ),
-                        "modelname": ("STRING",{"default": '', "multiline": False, "forceInput": True}),
+                        "modelname": ("STRING",{"default": '', "multiline": False, "forceInput": True},),
+                        "loras": ("STRING",{"default": "", "forceInput": False},),
                         "info": ("INFO",),
-                        "tags": ("STRING",{"default": "ai, comfyui, hyshare: ai", "forceInput": True}),
+                        "tags": ("STRING",{"default": "ai, comfyui, hyshare: ai", "forceInput": True},),
                     },
                     "hidden": {
                         "prompt": "PROMPT",
@@ -53,11 +84,10 @@ class Hydrus:
 
     CATEGORY = "Hydrus"
 
-    def wtf(self, images, positive="", negative="", modelname="", info={}, tags="", prompt=None, extra_pnginfo=None):
-        client = hydrus_api.Client(hydrus_api_key, hydrus_url)
+    def wtf(self, images, positive="", negative="", modelname="", loras="", info={}, tags="", prompt=None, extra_pnginfo=None):
+        client = get_hydrus_client()
         imagelist = []
         split = tags.split(',')
-        print("Prompt: {}".format(prompt))
         meta = []
         if positive != "":
             meta.append('positive: {}'.format(positive))
@@ -65,6 +95,9 @@ class Hydrus:
             meta.append('negative: {}'.format(negative))
         if modelname != "":
             meta.append('modelname: {}'.format(modelname))
+        if loras != []:
+            for lora in loras:
+                meta.append('lora: {}'.format(lora))
         if info != {}:
             meta.append('seed: {}'.format(info['Seed: ']))
             meta.append('steps: {}'.format(info['Steps: ']))
